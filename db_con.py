ad3a4dcdb9c5c062e41ea07d6d364c238f470813
NorbertasL <nlapenas@gmail.com>
2020-12-03 13:42:11 +0000

    Initial commit

---
+++ db_con.py
@@ -0,0 +1,63 @@
+import sqlite3
+import calendar;
+import time;
+
+connection = sqlite3.connect("L2RebornMarket.db")
+cursor = connection.cursor()
+
+SEEN_PRICE_TABLE_NAME = "SeenItemPrices"
+ITEM_NAME_LIST_TABLE_NAME = "ItemNameList"
+
+seenItemPriceTable = """CREATE TABLE IF NOT EXISTS "%s" (
+"_id"	INTEGER NOT NULL UNIQUE,
+"name"	TEXT NOT NULL,
+"seenAs"	TEXT NOT NULL,
+"price"	INTEGER NOT NULL,
+"quantity"	INTEGER NOT NULL,
+"date"	TEXT NOT NULL,
+"location"	TEXT,
+"person"	TEXT,
+PRIMARY KEY("_id" AUTOINCREMENT)
+);""" % SEEN_PRICE_TABLE_NAME
+
+itemNameListTable = """
+CREATE TABLE IF NOT EXISTS "%s" (
+"_id"	INTEGER NOT NULL UNIQUE,
+"name"	TEXT NOT NULL UNIQUE,
+"itemId"	INTEGER,
+PRIMARY KEY("_id" AUTOINCREMENT)
+);
+""" % ITEM_NAME_LIST_TABLE_NAME
+
+cursor.execute(seenItemPriceTable)
+cursor.execute(itemNameListTable)
+
+
+def addNewItem(name, itemId=-1):
+    try:
+        command = """INSERT INTO %s (name, itemId)
+        VALUES ('%s', %d)""" % (ITEM_NAME_LIST_TABLE_NAME, name, itemId)
+        cursor.execute(command)
+        connection.commit()
+    except sqlite3.IntegrityError as e:
+        print(e)
+        return e
+    return name, " added"
+
+
+def addSeenItem(name, seenAs, price, quantity, location, person):
+    ts = calendar.timegm(time.gmtime())
+    try:
+        command = "INSERT INTO %s VALUES (null, '%s', '%s', %d, %d, %d, '%s', '%s')" \
+                  % (SEEN_PRICE_TABLE_NAME, name, seenAs, price, quantity, ts, location, person)
+        cursor.execute(command)
+        connection.commit()
+    except sqlite3.IntegrityError as e:
+        return e
+
+    return name + " has been added, Price:"+str(price)
+
+
+def getAll(tableName):
+    cursor.execute("SELECT name, seenAs, price, quantity, date, location, person FROM "+tableName)
+    return cursor.fetchall()
---
+++ gui.py
@@ -0,0 +1,158 @@
+import time
+import tkinter as tk
+from datetime import datetime
+from tkinter import ttk
+import db_con
+
+window = tk.Tk("L2Reborn")
+window.geometry("1200x400")
+
+
+def addNewItem(name, itemId=-1):
+    # print("Name:", name, " id:", itemId)
+    db_con.addNewItem(name, itemId)
+
+
+# Using this to time click interval to prevent spam clicking
+lastTime = None
+def addNewPrice(name, seenAs, price, loc, quan, person):
+    timeNow = time.time()
+    global lastTime
+    global processLabel
+
+    if not name:
+        processLabel.config(text="Name is empty", fg="red")
+        return
+    if not seenAs:
+        processLabel.config(text="Seen as is empty", fg="red")
+        return
+    if price <= 0:
+        processLabel.config(text="Bad Price", fg="red")
+        return
+
+    if lastTime is None or timeNow - lastTime > 2:
+        returnValue = db_con.addSeenItem(name, seenAs, price, quan, loc, person)
+        lastTime = timeNow
+        processLabel.config(text=returnValue, fg="green")
+        updateList()
+    else:
+        print("Don't spam")
+        processLabel.config(text="Don't spam the button", fg="red")
+
+
+def openItemNameListWindow():
+    itemListWindow = tk.Toplevel()
+    itemListWindow.title("Item Window")
+    itemListWindow.geometry("500x100")
+
+    itemNameText = tk.StringVar()
+    mat_name_label = tk.Label(itemListWindow, text="Name:", font=('bold', 14), pady=20)
+    mat_name_label.grid(row=0, column=0, sticky='W')
+    mat_name_entry = tk.Entry(itemListWindow, textvariable=itemNameText)
+    mat_name_entry.grid(row=0, column=1, sticky='W', padx=20)
+
+    itemIdText = tk.IntVar(value=-1)
+    mat_id_label = tk.Label(itemListWindow, text="Item ID:", font=('bold', 14))
+    mat_id_label.grid(row=0, column=2, sticky='W')
+    mat_id_entry = tk.Entry(itemListWindow, textvariable=itemIdText, )
+    mat_id_entry.grid(row=0, column=3, sticky='W')
+
+    addBtn = tk.Button(itemListWindow, text="Add",
+                       command=lambda: addNewItem(itemNameText.get(), itemIdText.get()))
+    addBtn.grid(row=1, column=0)
+
+
+# Top Menu Bar
+my_menu = tk.Menu(window)
+window.config(menu=my_menu)
+add_menu = tk.Menu(my_menu)
+my_menu.add_cascade(label="Add", menu=add_menu)
+add_menu.add_command(label="New Item", command=addNewItem)
+add_menu.add_command(label="Item List", command=openItemNameListWindow)
+
+# New item entry widgets
+itemNameText = tk.StringVar()
+itemNameLabel = tk.Label(window, text="Name:", font=('bold', 14))
+itemNameLabel.grid(row=0, column=0, sticky='W')
+itemNameEntry = tk.Entry(window, textvariable=itemNameText)
+itemNameEntry.grid(row=0, column=1, sticky='W', padx=20)
+
+seenAsText = tk.StringVar(value="Sell")
+seenAsLabel = tk.Label(window, text="Seen As:", font=('bold', 14))
+seenAsLabel.grid(row=0, column=2, sticky="W")
+seenAsEntry = tk.Entry(window, textvariable=seenAsText)
+seenAsEntry.grid(row=0, column=3, sticky="W", padx=20)
+
+locationText = tk.StringVar(value="Giran")
+locationLabel = tk.Label(window, text="Loc:", font=('bold', 14))
+locationLabel.grid(row=0, column=4, sticky="W")
+locationEntry = tk.Entry(window, textvariable=locationText)
+locationEntry.grid(row=0, column=5, sticky="W")
+
+priceNameText = tk.IntVar(value=-1)
+priceNameLabel = tk.Label(window, text="Price:", font=('bold', 14))
+priceNameLabel.grid(row=1, column=0, sticky='W')
+priceNameEntry = tk.Entry(window, textvariable=priceNameText)
+priceNameEntry.grid(row=1, column=1, sticky='W', padx=20)
+
+quanText = tk.IntVar(value=1)
+quanLabel = tk.Label(window, text="Quantity:", font=('bold', 14))
+quanLabel.grid(row=1, column=2, sticky="W")
+quanEntry = tk.Entry(window, textvariable=quanText)
+quanEntry.grid(row=1, column=3, sticky="W", padx=20)
+
+personText = tk.StringVar()
+personLabel = tk.Label(window, text="Person:", font=('bold', 14))
+personLabel.grid(row=1, column=4, sticky="W")
+personEntry = tk.Entry(window, textvariable=personText)
+personEntry.grid(row=1, column=5, sticky="W")
+
+addBtn = tk.Button(window, text="Add", height=1, width=80,
+                   command=lambda: addNewPrice(itemNameText.get(), seenAsText.get(), priceNameText.get(),
+                                               locationText.get(), quanText.get(), personText.get()))
+addBtn.grid(row=3, column=0, columnspan=6)
+
+processLabel = tk.Label(window)
+processLabel.grid(row=4, column=2, columnspan=2)
+
+# Tree view
+myTree = ttk.Treeview(window)
+# Defining Columns
+myTree['columns'] = ("Name", "Seen As", "Price", "Quantity", "Date", "Location", "Person")
+# Formating columns
+myTree.column("#0", width=0, stretch="no")
+myTree.column("Name", anchor="w")
+myTree.column("Seen As", width=60, anchor="w")
+myTree.column("Price", width=200, anchor="c")
+myTree.column("Quantity", width=200, anchor="c")
+myTree.column("Date", anchor="c")
+myTree.column("Location", width=100, anchor="c")
+myTree.column("Person", width=100, anchor="c")
+
+# Headings
+myTree.heading("#0", text="")
+myTree.heading("Name", text="Name")
+myTree.heading("Seen As", text="Seen As")
+myTree.heading("Price", text="Price")
+myTree.heading("Quantity", text="Quantity")
+myTree.heading("Date", text="Date")
+myTree.heading("Location", text="Location")
+myTree.heading("Person", text="Person")
+
+myTree.grid(row=5, column=0, columnspan=6)
+
+
+def updateList():
+    myTree.delete(*myTree.get_children())
+    data = db_con.getAll(db_con.SEEN_PRICE_TABLE_NAME)
+    data.reverse()
+    count = 0
+    for record in data:
+        myTree.insert(parent='', index='end', iid=count, text='', values=(record[0], record[1], record[2], record[3],
+                                                                          datetime.fromtimestamp(int(record[4])), record[5],
+                                                                          record[6]))
+        count += 1
+
+
+updateList()
+window.mainloop()
