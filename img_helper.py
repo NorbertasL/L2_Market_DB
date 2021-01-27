311a3a31155d129d0d1aa6fbf8078c5924a7d1ed
NorbertasL <nlapenas@gmail.com>
2021-01-27 11:41:54 +0000

    Added a way to capture scree at mouse cursor

--- img_helper.py
+++ img_helper.py
@@ -1,5 +1,5 @@
-# Handles Taking the image and proceeding the text
 import mss
+import win32gui
 from PIL import ImageGrab
 import cv2
 import numpy as np
@@ -10,18 +10,24 @@ class ImageHelper:
     currentItemImage = None
 
     def getTargetNameImg(self):
-        global currentTargetImg
         img = ImageGrab.grab(bbox=(100,10,400,780))
         img_np = np.array(img)
-        currentTargetImg = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+        return cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+
+    def getItemInfoImg(self, point):
+        print(point[0]," ", point[1])
+        #So tha grap bbox par are X , Y , X+W, Y+H, what genius designed this?
+        #Stupides par i have ever seen for a image capturing
+        img_np = np.array(ImageGrab.grab(bbox=(point[0], point[1], point[0]+500, point[1]+500)))
+        return cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
 
 
-    def showImage(self):
-        cv2.imshow('test', currentTargetImg)
+
+    def showImage(self, img):
+        cv2.imshow('test', img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
 
 imgHelper = ImageHelper()
-imgHelper.getTargetNameImg()
-imgHelper.showImage()
+imgHelper.showImage(imgHelper.getItemInfoImg(win32gui.GetCursorPos()))
