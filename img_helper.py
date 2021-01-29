33d6b8fd5d19b1c6bc7449a3071857354fbca99e
NorbertasL <nlapenas@gmail.com>
2021-01-29 10:53:13 +0000

    Installed tesserackt

--- img_helper.py
+++ img_helper.py
@@ -3,25 +3,29 @@ import win32gui
 from PIL import ImageGrab
 import cv2
 import numpy as np
+import pytesseract
+
+class ImageHelper:
+    itemImage = None
+    nameImage = None
+
+    def getTargetNameImg(self):
+        img = ImageGrab.grab(bbox=(100, 10, 400, 780))
+        img_np = np.array(img)
+        return cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+
+    def getItemInfoImg(self, point):
+        print(point[0], " ", point[1])
+        # So tha grap bbox par are X , Y , X+W, Y+H, what genius designed this?
+        # Stupides par i have ever seen for a image capturing
+        img_np = np.array(ImageGrab.grab(bbox=(point[0], point[1], point[0] + 500, point[1] + 500)))
+        return cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
 
 
-def getTargetNameImg(self):
-    img = ImageGrab.grab(bbox=(100, 10, 400, 780))
-    img_np = np.array(img)
-    return cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-
-def getItemInfoImg(self, point):
-    print(point[0], " ", point[1])
-    # So tha grap bbox par are X , Y , X+W, Y+H, what genius designed this?
-    # Stupides par i have ever seen for a image capturing
-    img_np = np.array(ImageGrab.grab(bbox=(point[0], point[1], point[0] + 500, point[1] + 500)))
-    return cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+    def showImage(self, img):
+        cv2.imshow('test', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
-def showImage(self, img):
-    cv2.imshow('test', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-showImage(getItemInfoImg(win32gui.GetCursorPos()))
+    #showImage(getItemInfoImg(win32gui.GetCursorPos()))
