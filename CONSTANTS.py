7aa6ec7065c0f5f7d0538df3564d8dda08dc1bdd
NorbertasL <nlapenas@gmail.com>
2021-04-01 14:03:19 +0100

    Added some logging functions

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,10 +1,11 @@
 DEBUG_DIR = 'DEBUG'
 TESS_DEBUG_DIR_RAW = DEBUG_DIR + '/TESS/RAW_IMG'
 TESS_DEBUG_DIR_BW = DEBUG_DIR + '/TESS/BW_IMG'  # BW is Black and White
-LOGFILE_DIR = DEBUG_DIR + 'LOGS'
 
 # Debug log variables
 LOG = True
+LOG_NAME = "L2_Market_DB_Log.txt"
+LOGFILE = DEBUG_DIR + 'LOGS/' + LOG_NAME
 RAW_IMG_DATA = True
 BW_IMG_DATA = True
 RAW_TXT_DATA = True
--- DebugTools/debugg_log.py
+++ DebugTools/debugg_log.py
@@ -1,12 +1,20 @@
 import calendar
 import time
-
 import CONSTANTS
 
 
 def logRawImgData(nameImg, itemImg, ts=calendar.timegm(time.gmtime())):
-
     if CONSTANTS.RAW_IMG_DATA:
         # Saving raw img data
-        itemImg.save(CONSTANTS.TESS_DEBUG_DIR_RAW + '/Rew_Item ' + str(ts) + '.png')
-        nameImg.save(CONSTANTS.TESS_DEBUG_DIR_RAW + '/Raw_Name ' + str(ts) + '.png')
+        itemImgName = 'Rew_Item ' + str(ts) + '.png'
+        nameImgName = 'Raw_Name ' + str(ts) + '.png'
+        itemImg.save(CONSTANTS.TESS_DEBUG_DIR_RAW + '/' + itemImgName)
+        addLog("Created " + itemImgName, ts)
+        nameImg.save(CONSTANTS.TESS_DEBUG_DIR_RAW + '/' + nameImgName)
+        addLog("Created " + nameImgName, ts)
+
+def addLog(message, ts=calendar.timegm(time.gmtime())):
+    if CONSTANTS.LOG:
+        log = open(CONSTANTS.LOGFILE, 'a')
+        log.write(ts + ':' + message)
+        log.close()
