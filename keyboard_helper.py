c3179896a6d06bd7683dbda1b30ea5d6ecefeaa9
NorbertasL <nlapenas@gmail.com>
2021-04-02 11:25:38 +0100

    Switched to GlobalHotKeys to solve focus issue, but that didnt seem to make a differance

--- keyboard_helper.py
+++ keyboard_helper.py
@@ -1,38 +1,30 @@
-#https://pythonhosted.org/pynput/keyboard.html
-from pynput.keyboard import Listener, KeyCode
+# https://pythonhosted.org/pynput/keyboard.html
+from pynput.keyboard import Listener, KeyCode, GlobalHotKeys
 
 import CONSTANTS
 from ImgParseTools import img_capture
 
 
-def on_release(key):
-    print('{0} release'.format(
-        key))
-
-
-def on_press(key):
-    print('{0} pressed'.format(key))
-    if key == KeyCode.from_char(CONSTANTS.TAKE_PIC_KEY):
-        print("Gathering screen data...")
-        # Capturing img
-        img_capture.captureRawImg()
-
+def on_press():
+    #print('{0} pressed'.format(key))
+    #if key == KeyCode.from_char(CONSTANTS.TAKE_PIC_KEY):
+    print("Gathering screen data...")
+    # Capturing img
+    img_capture.captureRawImg()
 
 
 class KeyListener:
-    listener = None
+    listener = GlobalHotKeys({
+        CONSTANTS.TAKE_PIC_KEY: on_press
+    })
 
     def start(self):
-        self.listener = Listener(on_press=on_press, on_release=on_release, suppress=True)
+        #self.listener = Listener(on_press=on_press)
         self.listener.start()
+        print("Starting key listener:", self.listener)
         return self
 
     def stop(self):
         print("Stopping key listener:", self.listener)
         if self.listener is not None:
             self.listener.stop()
-            self.listener = None
-
-
-
-
