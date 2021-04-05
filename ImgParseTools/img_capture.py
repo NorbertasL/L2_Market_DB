8d7cb5874d12724fa36c6861a92a19a92dea1a6e
NorbertasL <nlapenas@gmail.com>
2021-04-05 13:57:50 +0100

    Switched to pyautogui.screenshot for image grabing. Im sorry but whoever designed ImageGrab.grab need to be exiled from the coding community.

--- ImgParseTools/img_capture.py
+++ ImgParseTools/img_capture.py
@@ -1,8 +1,8 @@
 import time
 
-import mss
+import pyautogui as pyautogui
 import win32gui
-from PIL import ImageGrab, Image
+from PIL import ImageGrab
 import cv2
 import numpy as np
 import pytesseract
@@ -49,9 +49,8 @@ def getText(saveData=True):
 
 # Grab the image of the name tag
 def getTargetNameImg():
-    return ImageGrab.grab(bbox=(CONSTANTS.NAME_LOC[0], CONSTANTS.NAME_LOC[1],
-                                CONSTANTS.NAME_LOC[0] + CONSTANTS.NAME_BOX_SIZE[0],
-                                CONSTANTS.NAME_LOC[1] + CONSTANTS.NAME_BOX_SIZE[1]))
+    return pyautogui.screenshot(region=(CONSTANTS.NAME_LOC[0], CONSTANTS.NAME_LOC[1],
+                                CONSTANTS.NAME_BOX_SIZE[0], CONSTANTS.NAME_BOX_SIZE[1]))
 
     # if saveImg:
     #    img.save(CONSTANTS.TESS_DEBUG_DIR+'/Name '+str(ts)+'.png')
@@ -64,7 +63,13 @@ def getItemInfoImg():
     # So tha grap bbox par are X , Y , X+W, Y+H, what genius designed this?
     # Stupides par i have ever seen for a image capturing
     point = win32gui.GetCursorPos()
-    return ImageGrab.grab(bbox=(point[0], point[1], point[0] + 500, point[1] + 500))
+    # NOTE: The offset numbers are here, because to get item the mouse needs to be anywhere in the item box,
+    # but the info window always starts from the top left cornet(it invert flips if space is an issue, but
+    # im not going to deal with it any time soon)
+    return pyautogui.screenshot(region=(point[0]-40, point[1] - CONSTANTS.ITEM_BOX_SIZE[1],
+                                        CONSTANTS.ITEM_BOX_SIZE[0], CONSTANTS.ITEM_BOX_SIZE[1]))
+    #return ImageGrab.grab(bbox=(point[0], point[1],
+    #                            point[0] + 200, point[1] + 200))
 # ts = calendar.timegm(time.gmtime())
 # if saveImg:
 #     img.save(CONSTANTS.TESS_DEBUG_DIR+'/Item '+str(ts)+'.png')
