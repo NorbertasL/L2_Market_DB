92ba706fc54d5b5965ccde3d12671560bd1af5e3
NorbertasL <nlapenas@gmail.com>
2021-04-13 12:31:05 +0100

    Scrip for scaling .png file for easier tesseract training

---
+++ SAMPLES/TESS/TRAINING/image_scale_up.py
@@ -0,0 +1,15 @@
+# Script that scales image up
+# Will take all png giles from "currentDir"\OriginalSize
+# And scale them up + output them to "currentDir" with the same file name
+
+import os
+from PIL import Image
+
+path = os.path.dirname(os.path.abspath(__file__)) + "\OriginalSize"
+print("Getting all .png from ", path)
+for file in os.listdir(path):
+    if file.endswith(".png"):
+        scaling = 4  # 4 time image size
+        img = Image.open(path + "\\" + file)
+        img = img.resize((int(img.size[0]) * scaling, int(img.size[1]) * scaling), Image.ANTIALIAS)
+        img.save(file)
